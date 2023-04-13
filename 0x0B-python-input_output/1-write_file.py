321412d9dff9cee2755a9cbdbc8f498180c54a50
dismond5 <omarkhaled201148@gmail.com>
2023-04-13 14:57:46 -0700

    a

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,9 +1,16 @@
 #!usr/bin/python3
-"""contains a function that writes a text files"""
+"""Module containing the function write_file"""
 
 
 def write_file(filename="", text=""):
-    """ writes a string to a text file(UTF8) and returns
-    the number of characters written."""
-    with open(filename, 'w') as f:
+    """Writes a string to a text file (UTF8) and returns the number
+    of characters written.
+    Args:
+        filename (str, optional): name of the file. Defaults to "".
+        text (str, optional): string of text to write to file. Defaults to "".
+    Returns:
+        int: number of characters written to file.
+    """
+    with open(filename, 'w', encoding="utf-8") as f:
+        """This method returns the number of characters written to a file."""
         return f.write(text)
--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -5,5 +5,5 @@
 def append_write(filename="", text=""):
     """ appends a string at the end of a text file (UTF8)
     and returns the number of characters"""
-    with open(filename, "a") as f:
-        return f,write(test)
+    with open(filename, "a", encoding="utf-8") as f:
+        return f.write(test)
